599820d56f4e504a77ead03a8bd796d5469aa238
Sanzhar Kushekbayev <sanzhar.kushekbayev@gmail.com>
2019-03-27 14:07:19 +1100

    ability to create new post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends Component {
 
@@ -24,15 +27,7 @@ class PostForm extends Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts',{
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    this.props.createPost(post);
   }
 
   render() {
@@ -54,7 +49,10 @@ class PostForm extends Component {
       </div>
     );
   }
-
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(PostForm);
